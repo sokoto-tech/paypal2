b0eba243fdbb0c5cb17c66c0826b1a96a81b51ec
emiltekuh <108840159+emiltekuh@users.noreply.github.com>
2023-02-28 18:44:21 -0500

    Update file2.sh
    online info added

---
+++ file2.sh
@@ -0,0 +1,4 @@
+Turn Your Computer or Phone Into a Terminal That Accepts Remote Payments. 
+Find a Smart Mobile Payments Solution That Fits Your Business. Sign up for Square Today!
+
+Square Checking + Payment · Square® Subscriptions · Save With Square Savings
